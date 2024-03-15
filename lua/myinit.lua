33763c5e998cbfc53c3ff81e3194100e1e99da07
Vasilis Christopoulos <v.christopoulos@divbyzero.gr>
2024-03-15 17:34:54 +0200

    fix: removed archive plugin and migrate it to typescript-tools
    Removed the auto cmd from init that caused error message on save of js and ts files

--- lua/myinit.lua
+++ lua/myinit.lua
@@ -1,33 +1,10 @@
 -- local autocmd = vim.api.nvim_create_autocmd
 
--- Auto resize panes when resizing nvim window
--- autocmd("VimResized", {
---   pattern = "*",
---   command = "tabdo wincmd =",
--- })
-
-local Format = vim.api.nvim_create_augroup("Format", { clear = true })
-
 vim.api.nvim_create_autocmd("BufWritePre", {
-	group = Format,
-	pattern = "*.tsx,*.ts,*.jsx,*.js",
-	callback = function()
-		if vim.fn.exists(":TypescriptFixAll") then
-			vim.cmd("TypescriptFixAll!")
-		end
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
 	end,
 })
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
-  end,
-})
-
--- Copilot
--- vim.g.copilot_no_tab_map = true
--- vim.g.copilot_assume_mapped = true
--- vim.g.copilot_tab_fallback = ""
 
 vim.opt.conceallevel = 1
--- lua/plugins/myplugins.lua
+++ lua/plugins/myplugins.lua
@@ -222,7 +222,8 @@ local plugins = {
 	},
 
 	{
-		"jose-elias-alvarez/typescript.nvim",
+		"pmizio/typescript-tools.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
 		event = "LspAttach",
 		config = function()
 			require("configs.typescript")
