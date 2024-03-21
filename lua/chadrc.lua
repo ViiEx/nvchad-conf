a543edaf00539dad95fe563275f7d916ee04e92a
Vasilis Christopoulos <v.christopoulos@divbyzero.gr>
2024-03-21 16:55:15 +0200

    chore: format

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -2,67 +2,69 @@
 local M = {}
 
 -- Path to overriding theme and highlights files
-local highlights = require "highlights"
+local highlights = require("highlights")
 
 M.ui = {
-  theme = "pastelbeans",
-  theme_toggle = { "pastelbeans", "one_light" },
+	theme = "yoru",
+	theme_toggle = { "yoru", "one_light" },
 
-  hl_override = highlights.override,
-  hl_add = highlights.add,
+	hl_override = highlights.override,
+	hl_add = highlights.add,
 
-  cmp = {
-    icons = true,
-    style = "atom",
-    selected_item_bg = "simple",
-  },
+	cmp = {
+		icons = true,
+		style = "atom",
+		selected_item_bg = "simple",
+	},
 
-  nvdash = {
-    load_on_startup = true,
-    header = {
-      "                                   ",
-      "   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ",
-      "    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ",
-      "          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ",
-      "           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ",
-      "          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ",
-      "   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ",
-      "  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ",
-      " ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ",
-      " ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ",
-      "      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ",
-      "       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
-      "                                   ",
-    },
-  },
+	nvdash = {
+		load_on_startup = true,
+		header = {
+			"                                   ",
+			"   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ",
+			"    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ",
+			"          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄     ",
+			"           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ",
+			"          ⢠⣿⣿⣿⠈    ⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ",
+			"   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ",
+			"  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ",
+			" ⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ",
+			" ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇ ⠛⠻⢷⣄ ",
+			"      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ",
+			"       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
+			"                                   ",
+		},
+	},
 
-  statusline = {
-    overriden_modules = function(modules)
-      table.insert(
-        modules,
-        9,
-        (function()
-          --- @diagnostic disable: deprecated
-          for _, client in pairs(vim.lsp.get_active_clients()) do
-            if client.name == "copilot" then
-              return "%#TeSTTCopilot#" .. "   "
-            end
-          end
-          return "%#TeSTTCopilot#" .. "   "
-        end)()
-      )
-    end,
-    theme = "vscode_colored",
-  },
+	statusline = {
+		-- NOTE: Overide modules in statusline seems to be removed for now.
+		-- I will keep this here for reference.
+		-- NOTE: Try to find a different way to override the statusline
+		overriden_modules = function(modules)
+			table.insert(
+				modules,
+				9,
+				(function()
+					--- @diagnostic disable: deprecated
+					for _, client in pairs(vim.lsp.get_active_clients()) do
+						if client.name == "copilot" then
+							return "%#TeSTTCopilot#" .. "   "
+						end
+					end
+					return "%#TeSTTCopilot#" .. "   "
+				end)()
+			)
+		end,
+		theme = "vscode_colored",
+	},
 
-  telescope = {
-    style = "borderless",
-  },
+	telescope = {
+		style = "borderless",
+	},
 }
 
 -- M.plugins = "plugins"
 
 -- check core.mappings for table structure
 -- M.mappings = require "mappings"
-
 return M
