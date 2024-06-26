1ea40010f2a5c1a406ac8b1f49d48e26d32e8789
Vasilis Christopoulos <vaschristopoulos@gmail.com>
2024-06-26 23:51:09 +0300

    feat: add neorg

--- lua/configs/norg.lua
+++ lua/configs/norg.lua
@@ -1,39 +1,44 @@
-local status_ok, norg = pcall(require, "norg")
+local status_ok, neorg = pcall(require, "neorg")
 
 if not status_ok then
-  return
+	return
 end
 
 local opts = {
-  load = {
-    ["core.defaults"] = {},
-    ["core.itero"] = {},
-    ["core.keybinds"] = {},
-    ["core.norg.concealer"] = {},
-    ["core.concealer"] = {},
-    ["core.dirman"] = {
-      config = {
-        workspaces = {
-          notes = "~/notes",
-        },
-        default_workspace = "notes",
-      },
-    },
-    ["core.norg.completion"] = {
-      config = {
-        engine = "nvim-cmp",
-      },
-    },
-    ["core.export"] = {},
-    ["core.export.markdown"] = {
-      config = {
-        extensions = "all",
-      },
-    },
-  },
+	load = {
+		["core.defaults"] = {},
+		["core.itero"] = {},
+		["core.keybinds"] = {},
+		-- ["core.neorg.concealer"] = {},
+		["core.concealer"] = {},
+		["core.dirman"] = {
+			config = {
+				workspaces = {
+					notes = "~/notes",
+				},
+				default_workspace = "notes",
+			},
+		},
+		["core.completion"] = {
+			config = {
+				engine = "nvim-cmp",
+			},
+		},
+		["core.export"] = {},
+		["core.export.markdown"] = {
+			config = {
+				extensions = "all",
+			},
+		},
+		["core.neorgcmd"] = {},
+		["core.highlights"] = {},
+		["core.autocommands"] = {},
+		["core.mode"] = {},
+		["core.integrations.treesitter"] = {},
+	},
 }
 
-norg.setup(opts)
+neorg.setup(opts)
 
 vim.wo.foldlevel = 99
 vim.wo.conceallevel = 2
