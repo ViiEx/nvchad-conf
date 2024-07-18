7e09418d229b4ba4aa652dac70e22d72afbb9de4
Vasilis Christopoulos <v.christopoulos@divbyzero.gr>
2024-07-18 12:26:28 +0300

    chore(neorg): update config based on changes of v9.0.0

--- lua/plugins/norg.lua
+++ lua/plugins/norg.lua
@@ -1,13 +1,6 @@
 return {
 	{
-		"vhyrro/luarocks.nvim",
-		priority = 1000, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
-		config = true,
-		event = "VimEnter",
-	},
-	{
 		"nvim-neorg/neorg",
-		dependencies = { "luarocks.nvim" },
 		lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
 		version = "*", -- Pin Neorg to the latest stable release
 		config = function()
