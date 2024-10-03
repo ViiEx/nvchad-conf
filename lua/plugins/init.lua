5cbfc780fe5641f330b57a13d0fee7328d1e388c
Vasilis Christopoulos <v.christopoulos@divbyzero.gr>
2024-10-03 11:32:29 +0300

    feat: add markview

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -108,4 +108,18 @@ return {
   --  "hrsh7th/nvim-cmp",
   --  opts = require "configs.nvim-cmp",
   --},
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false, -- Recommended
+    -- ft = "markdown" -- If you decide to lazy-load anyway
+
+    dependencies = {
+      -- You will not need this if you installed the
+      -- parsers manually
+      -- Or if the parsers are in your $RUNTIMEPATH
+      "nvim-treesitter/nvim-treesitter",
+
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
 }
